18a49587ca66575fa2e0afd2c9fba8fe9a453c68
Hernan Rubio <hernan.andres.rubio@gmail.com>
2023-10-27 14:57:27 -0300

    first commit

---
+++ src/test/java/com/unnoba/practica3/Practica3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unnoba.practica3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
